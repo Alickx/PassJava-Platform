264d29ba9570a36240c7974bbcb676f13868a2d2
Alickx <89991829@qq.com>
2022-02-24 18:03:01 +0800

    初始化

---
+++ passjava-member/src/main/java/cn/goroute/passjava/member/PassjavaMemberApplication.java
@@ -0,0 +1,13 @@
+package cn.goroute.passjava.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassjavaMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassjavaMemberApplication.class, args);
+    }
+
+}
